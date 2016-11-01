753788611cef0f94149ee3da9f75daf56979a0e7
trueivorian <ocg14@ic.ac.uk>
2016-11-01 23:01:46 +0000

    Finished Player Walking Scripts (With Animations)

--- Character Scripts/Player.cs
+++ Character Scripts/Player.cs
@@ -7,26 +7,15 @@ public class Player : Character {
 	public static Player instance;
 
 	// Player movement parameters
-	// Private:
-	public float playerSpeed;
-	public float playerMoveDistance;
+	private float playerSpeed;
+	private float playerMoveDistance;
 	private float moveError;
 	private float moveTime;
-	private bool isMoving = false;
+	private bool isMoving;
 	private float currentDirection;
 
-	// Fixed directions of player travel (based on the angle ratios we're using for the map)
-	private const float N = 0.663225f,
-	NE = 0.331613f,
-	E = 0.0f,
-	SE = 0.663225f + Mathf.PI + 0.331613f,
-	S = 0.663225f + Mathf.PI,
-	SW = 0.663225f + Mathf.PI - 0.331613f,
-	W = Mathf.PI,
-	NW = 0.663225f + 0.331613f;
-
 	// Called on script instance creation
-	void Awake(){
+	private void Awake(){
 
 		// Assign the current instance of the script to this variable if one hasn't been created
 		if (instance == null) {
@@ -35,31 +24,36 @@ public class Player : Character {
 
 		// Initialise player components
 		this.anim = this.GetComponent<Animator> ();
+		//this.animState = this.GetComponent<Animation> ();
 		this.myBody = this.GetComponent<Rigidbody2D> ();
 		this.moveError = 0.2f;
 		this.moveTime = 0.0f;
 		this.isMoving = false;
-		this.currentDirection = E;
+		this.currentDirection = RainerisConstants.E;
 		this.playerSpeed = 5.0f;
 		this.playerMoveDistance = 5.0f;
 
 	}
 
 	// Update is called once per frame
-	void Update	() {
+	private void Update	() {
 
 		move ();
+
+	}
+
+	private void FixedUpdate(){
 
 	}
 
 	// Translate the player in a specific direction
 	private void move(){
 		
-		moveStart ();
-
+//		moveStart ();
+//
 		chooseDirection ();
-
-		moving (currentDirection);
+//
+//		moving (currentDirection);
 
 	}
 
@@ -80,7 +74,7 @@ public class Player : Character {
 
 		if (Input.GetKeyDown ("up")) {
 			anim.SetBool ("upPressed", true);
-			this.currentDirection = N;
+			this.currentDirection = RainerisConstants.N;
 		}
 
 		if (Input.GetKeyUp ("up")) {
@@ -89,7 +83,7 @@ public class Player : Character {
 
 		if (Input.GetKeyDown ("down")) {
 			anim.SetBool ("downPressed", true);
-			this.currentDirection = S;
+			this.currentDirection = RainerisConstants.S;
 		}
 
 		if (Input.GetKeyUp ("down")) {
@@ -98,7 +92,7 @@ public class Player : Character {
 
 		if (Input.GetKeyDown ("left")) {
 			anim.SetBool ("leftPressed", true);
-			this.currentDirection = W;
+			this.currentDirection = RainerisConstants.W;
 		}
 
 		if (Input.GetKeyUp ("left")) {
@@ -107,7 +101,7 @@ public class Player : Character {
 
 		if (Input.GetKeyDown ("right")) {
 			anim.SetBool ("rightPressed", true);
-			this.currentDirection = E;
+			this.currentDirection = RainerisConstants.E;
 		}
 
 		if (Input.GetKeyUp ("right")) {
@@ -117,27 +111,27 @@ public class Player : Character {
 	}
 
 	// Move the player object until they reach the playerMoveDistance
-	private void moving(float direction){
+	public void movePlayer(float direction){
 
-		if (isMoving) {
-			
-			moveTime += Time.deltaTime;		
+		Vector3 moveVector = new Vector2 (playerSpeed * Mathf.Cos (direction), playerSpeed * Mathf.Sin (direction));
 
-			Vector3 moveVector = new Vector3 (playerSpeed * Mathf.Cos (direction), playerSpeed * Mathf.Sin (direction), 0);
+		this.myBody.velocity = new Vector2 (moveVector.x, moveVector.y);
 
-			this.myBody.velocity = new Vector2 (moveVector.x, moveVector.y);
+		//Debug.Log ("Moving " + (180.0f * direction) / Mathf.PI + " degrees");
 
-			float moveDistance = playerSpeed * moveTime;
+	}
 
-			if (moveDistance >= this.playerMoveDistance) {
-				this.isMoving = false;
-				moveTime = 0.0f;
-				Debug.Log ("Stopped Moving");
-			}
+	// Move the player object by a specified speed until they reach the playerMoveDistance
+	public void movePlayer(float speed, float direction){
+		
+		Vector3 moveVector = new Vector2 (speed * Mathf.Cos (direction), speed * Mathf.Sin (direction));
 
-			Debug.Log ("Moving " + (180.0f * direction) / Mathf.PI + " degrees");
+		this.myBody.velocity = new Vector2 (moveVector.x, moveVector.y);
+	}
 
-		}
+	public float getCurrentDirection(){
+
+		return currentDirection;
 
 	}
 
