5bf9fe9e75f281c513063dabc7bc87d3eb839be6
Hong Koh <hong.koh16@imperial.ac.uk>
2016-11-03 19:53:30 +0000

    Testing admin changes

--- Animal Scripts/Animal.cs
+++ Animal Scripts/Animal.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+//test
 public abstract class Animal : MonoBehaviour, IAttackable {
 
 	protected Rigidbody2D myBody;
