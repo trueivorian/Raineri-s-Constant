ca2d2446c798647cf72eec17b876be4021d97bed
trueivorian <ocg14@ic.ac.uk>
2016-11-10 13:58:14 +0000

    Updated Pig and JobQueue

--- Animal Scripts/Pig.cs
+++ Animal Scripts/Pig.cs
@@ -8,7 +8,6 @@ public class Pig : Animal {
 	private Pig pigInstance;
 	private float pigSpeed;
 	private float currentDirection;
-	//private delegate void job();
 
 	// Use this for initialization
 	void Awake () {
@@ -23,30 +22,38 @@ public class Pig : Animal {
 		this.health = new Health (100.0f);
 		this.pigSpeed = 5.0f;
 		this.currentDirection = Direction.E;
-		this.animalJobQueue.setIsWorking(true);
+		this.animalJobQueue.setIsWorking (true);
 
-		// Attempt to simulate 4 up button presses
-		this.animalJobQueue.addJob (delegate() {this.anim.SetBool ("upPressed", true);});
-		this.animalJobQueue.addJob (delegate() {this.anim.SetBool ("upPressed", true);});
-		this.animalJobQueue.addJob (delegate() {this.anim.SetBool ("upPressed", true);});
-		this.animalJobQueue.addJob (delegate() {this.anim.SetBool ("upPressed", true);});
+		for (int i = 0; i < 50; i++) {
+			this.animalJobQueue.addJob (() => {
+				this.anim.SetBool ("upPressed", true);
+			});
+		}
+
+		for (int i = 0; i < 50; i++) {
+			this.animalJobQueue.addJob (() => {
+				this.anim.SetBool ("upPressed", false);
+				this.anim.SetBool ("downPressed", true);
+			});
+		}
+
+		this.animalJobQueue.addJob (() => {
+			this.anim.SetBool ("downPressed", false);
+		});
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-
 		// TODO: Invoking methods from a queue
-		//pigInstance.anim.SetBool ("upPressed", true);
-		this.animalJobQueue.addJob (delegate() {pigInstance.anim.SetBool ("upPressed", true);});
 		this.animalJobQueue.work ();
 	}
 
-	public Pig getPigInstance(){
+	public Pig getPigInstance () {
 		return this.pigInstance;
 	}
 
 	// Move the pig object
-	public void movePig(float speed, float direction){
+	public void movePig (float speed, float direction){
 
 		Vector2 moveVector = new Vector2 (speed * Mathf.Cos (direction), speed * Mathf.Sin (direction));
 
@@ -54,17 +61,17 @@ public class Pig : Animal {
 	}
 
 	// Move the pig object
-	public void movePig(float direction){
+	public void movePig (float direction){
 		movePig (pigSpeed, direction);
 	}
 
-	public void stopPig(){
+	public void stopPig (){
 		movePig (0.0f, currentDirection);
 	}
 
-	public float getCurrentDirection(){
+	public float getCurrentDirection (){
 		return currentDirection;
 	}
 
-    
+
 }
--- Type Scripts/JobQueue.cs
+++ Type Scripts/JobQueue.cs
@@ -3,50 +3,51 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-// Code Source: http://stackoverflow.com/questions/10717344/c-sharp-creating-function-queue
 public class JobQueue {
-    private Queue jobQueue;
-    private bool isWorking;
 
-    public JobQueue () {
-        this.jobQueue = new Queue();
-        this.isWorking = false;
-    }
+	private Queue<Action> jobQueue;
+	private bool isWorking;
 
-    public void work () {
-        if (this.isWorking) {
-            this.doJob();
-        }
-    }
+	public JobQueue(){
+		this.jobQueue = new Queue<Action>();
+		this.isWorking = false;
+	}
 
-    public bool isJobless {
-        get {
-            if (jobQueue.Count == 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }
+	public void work(){
+		if (this.isWorking) {
+			this.doJob ();
+		}
+	}
 
-    public bool contains (Action job) {
-        if (jobQueue.Contains(job)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public bool isJobless(){
+		if (jobQueue.Count == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public Action doJob () {
-        Debug.Log("Working");
-        return jobQueue.Dequeue() as Action;
-    }
+	public bool contains(Action job){
+		if (jobQueue.Contains (job)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-    public void addJob (Action job) {
-        jobQueue.Enqueue(job);
-    }
+	public void doJob(){
+		if (!this.isJobless ()) {
+			(jobQueue.Dequeue ()) ();
+		} else {
+			Debug.Log ("Empty Queue");
+		}
+	}
 
-    public void setIsWorking (bool _isWorking) {
-        this.isWorking = _isWorking;
-    }
+	public void addJob(Action job){
+		jobQueue.Enqueue (job);
+	}
+
+	public void setIsWorking(bool _isWorking){
+		this.isWorking = _isWorking;
+	}
 }
